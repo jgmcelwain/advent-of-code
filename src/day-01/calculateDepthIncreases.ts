3238f4ba9cc39a5b27eef9b9ddd94daac9272eed
Jamie McElwain <jgmcelwain@icloud.com>
2021-12-02 11:56:26 +0000

    refactor: rename windowMeasurements to windowSums

--- src/day-01/calculateDepthIncreases.ts
+++ src/day-01/calculateDepthIncreases.ts
@@ -2,6 +2,7 @@ import type { DepthReport } from '.';
 
 export function calculateDepthIncreases(depthReport: DepthReport): number {
   let depthIncreases = 0;
+
   for (let i = 0; i < depthReport.length; i++) {
     if (depthReport[i] > depthReport[i - 1]) {
       depthIncreases++;
--- src/day-01/calculateWindowSumIncreases.ts
+++ src/day-01/calculateWindowSumIncreases.ts
@@ -2,13 +2,11 @@ import type { DepthReport } from '.';
 import { calculateDepthIncreases } from './calculateDepthIncreases';
 
 export function calculateWindowSumIncreases(depthReport: DepthReport): number {
-  const windowMeasurements: number[] = [];
+  const windowSums: number[] = [];
 
   for (let i = 0; i < depthReport.length - 2; i++) {
-    windowMeasurements.push(
-      depthReport[i] + depthReport[i + 1] + depthReport[i + 2],
-    );
+    windowSums.push(depthReport[i] + depthReport[i + 1] + depthReport[i + 2]);
   }
 
-  return calculateDepthIncreases(windowMeasurements);
+  return calculateDepthIncreases(windowSums);
 }
