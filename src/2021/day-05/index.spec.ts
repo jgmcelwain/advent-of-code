5ef4f3901eee8ceaab77f4a0aeab5960c41546db
Jamie McElwain <jgmcelwain@icloud.com>
2021-12-06 11:53:13 +0000

    refactor: import LineKind instead of hard coding enum values

--- src/2021/day-05/index.spec.ts
+++ src/2021/day-05/index.spec.ts
@@ -1,17 +1,18 @@
-import type { Line } from './index';
+import type { Line } from '.';
+import { LineKind } from '.';
 import { getPositionTraversedCounts } from './getPositionTraversedCounts';
 
 const exampleLines: Line[] = [
-  { start: { x: 0, y: 9 }, end: { x: 5, y: 9 }, kind: 0 },
-  { start: { x: 8, y: 0 }, end: { x: 0, y: 8 }, kind: 2 },
-  { start: { x: 9, y: 4 }, end: { x: 3, y: 4 }, kind: 0 },
-  { start: { x: 2, y: 2 }, end: { x: 2, y: 1 }, kind: 1 },
-  { start: { x: 7, y: 0 }, end: { x: 7, y: 4 }, kind: 1 },
-  { start: { x: 6, y: 4 }, end: { x: 2, y: 0 }, kind: 2 },
-  { start: { x: 0, y: 9 }, end: { x: 2, y: 9 }, kind: 0 },
-  { start: { x: 3, y: 4 }, end: { x: 1, y: 4 }, kind: 0 },
-  { start: { x: 0, y: 0 }, end: { x: 8, y: 8 }, kind: 2 },
-  { start: { x: 5, y: 5 }, end: { x: 8, y: 2 }, kind: 2 },
+  { start: { x: 0, y: 9 }, end: { x: 5, y: 9 }, kind: LineKind.Horizontal },
+  { start: { x: 8, y: 0 }, end: { x: 0, y: 8 }, kind: LineKind.Diagonal },
+  { start: { x: 9, y: 4 }, end: { x: 3, y: 4 }, kind: LineKind.Horizontal },
+  { start: { x: 2, y: 2 }, end: { x: 2, y: 1 }, kind: LineKind.Vertical },
+  { start: { x: 7, y: 0 }, end: { x: 7, y: 4 }, kind: LineKind.Vertical },
+  { start: { x: 6, y: 4 }, end: { x: 2, y: 0 }, kind: LineKind.Diagonal },
+  { start: { x: 0, y: 9 }, end: { x: 2, y: 9 }, kind: LineKind.Horizontal },
+  { start: { x: 3, y: 4 }, end: { x: 1, y: 4 }, kind: LineKind.Horizontal },
+  { start: { x: 0, y: 0 }, end: { x: 8, y: 8 }, kind: LineKind.Diagonal },
+  { start: { x: 5, y: 5 }, end: { x: 8, y: 2 }, kind: LineKind.Diagonal },
 ];
 
 describe('getPositionTraversedCounts', () => {
