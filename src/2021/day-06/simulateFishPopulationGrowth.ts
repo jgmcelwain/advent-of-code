6427564bf083a919b9913f4bb493b8983c43ca0f
Jamie McElwain <jgmcelwain@icloud.com>
2021-12-06 10:28:05 +0000

    refactor: prefer forEach to a for loop

--- src/2021/day-06/simulateFishPopulationGrowth.ts
+++ src/2021/day-06/simulateFishPopulationGrowth.ts
@@ -2,10 +2,8 @@ export function simulateFishPopulationGrowth(
   startingFish: number[],
   daysToSimulate: number,
 ) {
-  let fishStates: number[] = Array(9).fill(0);
-  for (let i = 0; i < startingFish.length; i++) {
-    fishStates[startingFish[i]]++;
-  }
+  let fishStates = Array<number>(9).fill(0);
+  startingFish.forEach((fishState) => fishStates[fishState]++);
 
   for (let day = 0; day < daysToSimulate; day++) {
     const [reproducing, ...nextGeneration] = fishStates;
