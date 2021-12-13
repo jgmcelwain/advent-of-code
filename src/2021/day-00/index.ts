54e7e85c0246a26b0edf3e37f737eece1873a32a
Jamie McElwain <jgmcelwain@icloud.com>
2021-12-13 13:54:32 +0000

    feat: boilerplate day to copy

---
+++ src/2021/day-00/index.ts
@@ -0,0 +1,27 @@
+import { getInput } from '../../../lib/getInput';
+import { runDay } from '../../../lib/runDay';
+
+function partOne() {
+  return null;
+}
+
+function partTwo() {
+  return null;
+}
+
+async function main() {
+  const input = await getInput(__dirname);
+
+  runDay(
+    0,
+    0,
+    '',
+    () => partOne(),
+    () => partTwo(),
+    true,
+  );
+}
+
+if (process.argv.includes('run')) {
+  main();
+}
