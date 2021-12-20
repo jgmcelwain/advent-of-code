8f94ab53ca010302dd50d79fddfdc65ffc406f36
Jamie McElwain <jgmcelwain@icloud.com>
2021-12-20 16:11:41 +0000

    refactor: more descriptive variable names when computing iteration default

--- src/2021/day-20/enhanceImage.ts
+++ src/2021/day-20/enhanceImage.ts
@@ -8,16 +8,19 @@ export function enhanceImage(
   // enhancement maps that start with a '1' will cause the "border" of the
   // image, where new pixels have to be generated, to alternate between '1' and
   // '0'.
+  const isAlternatingInfiniteMap = enhancementMap[0] === '1';
+  const isOddIteration = iterationNumber % 2 === 1;
   const iterationDefault: Pixel =
-    enhancementMap[0] === '1' && iterationNumber % 2 === 1 ? '1' : '0';
+    isAlternatingInfiniteMap && isOddIteration ? '1' : '0';
 
-  const output: Pixel[][] = [];
+  const output: Image = [];
 
   for (let y = -1; y <= image.length; y++) {
     const outputRow: Pixel[] = [];
 
     for (let x = -1; x <= image.length; x++) {
       let enhancementMapIndex = '';
+
       for (let dy = -1; dy <= 1; dy++) {
         for (let dx = -1; dx <= 1; dx++) {
           enhancementMapIndex += image[y + dy]?.[x + dx] ?? iterationDefault;
