40ec3d9608b7fa0e9cb928b81d7bb1102bbe7cd1
Jamie McElwain <jgmcelwain@icloud.com>
2022-12-05 11:04:03 +0000

    feat: add an option to disable trimming when getting input

--- lib/getInput.ts
+++ lib/getInput.ts
@@ -1,10 +1,10 @@
 import { readFile } from 'fs/promises';
 import path from 'path';
 
-export async function getInput(dirname: string) {
+export async function getInput(dirname: string, trim = true) {
   const result = await readFile(path.join(dirname, 'input.txt'), {
     encoding: 'utf-8',
   });
 
-  return result.trim();
+  return trim ? result.trim() : result;
 }
